f63ea6e5d0df39eb5d7fb063bd7bf0d98775d06f
Williams de SOUZA <williamsko89@gmail.com>
2021-06-08 01:23:04 +0200

    key model

--- api/resources/login_resource.py
+++ api/resources/login_resource.py
@@ -87,7 +87,7 @@ class AgentResource(ModelResource):
         data = self.deserialize(request, request.body)
         key = data['key']
         try:
-            existing_key = Key.objects.get(key=key)
+            existing_key = Key.objects.get(content=key)
             if existing_key.status:
                 return self.create_response(request, {'response_text': 'key already used', 'response_code': '100'}, HttpForbidden)
 
